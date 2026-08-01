b7d5e19656086afe6d02bd8364a902a6b2349e8e
Paki <53056381+Plikie@users.noreply.github.com>
2026-08-01 23:10:31 +0200

    Use shared NPC manager registry and trait for Guild Grandmaster

--- src/main/java/com/ebicep/warlords/guilds/shop/GuildGrandmasterManager.java
+++ src/main/java/com/ebicep/warlords/guilds/shop/GuildGrandmasterManager.java
@@ -1,11 +1,24 @@
 package com.ebicep.warlords.guilds.shop;
 
+import com.ebicep.customentities.npc.NPCManager;
+import com.ebicep.customentities.npc.traits.GuildGrandmasterTrait;
+import com.ebicep.holograms.Hologram;
+import com.ebicep.holograms.HologramDataText;
+import com.ebicep.holograms.HologramManager;
+import com.ebicep.holograms.VisibilityType;
 import com.ebicep.warlords.Warlords;
+import com.ebicep.warlords.database.leaderboards.stats.StatsLeaderboardManager;
+import com.ebicep.warlords.util.bukkit.ComponentBuilder;
 import com.ebicep.warlords.util.bukkit.ItemBuilder;
+import net.citizensnpcs.api.npc.NPC;
+import net.citizensnpcs.trait.LookClose;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.entity.Display;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -26,11 +39,42 @@ public final class GuildGrandmasterManager implements Listener {
                     Component.text("Click to visit the Guild Grandmaster shop.", NamedTextColor.YELLOW)
             )
             .get();
+    private boolean initialized;
 
     private GuildGrandmasterManager() {
     }
 
     public void init() {
+        if (!Warlords.citizensEnabled || initialized) {
+            return;
+        }
+        initialized = true;
+
+        NPCManager.registerTrait(GuildGrandmasterTrait.class, "GuildGrandmasterTrait");
+
+        NPC npc = NPCManager.NPC_REGISTRY.createNPC(EntityType.VILLAGER, "guild-grandmaster");
+        npc.addTrait(GuildGrandmasterTrait.class);
+
+        LookClose lookClose = npc.getOrAddTrait(LookClose.class);
+        lookClose.setPerPlayer(true);
+        lookClose.toggle();
+
+        npc.data().set(NPC.Metadata.VILLAGER_BLOCK_TRADES, true);
+        npc.data().set(NPC.Metadata.NAMEPLATE_VISIBLE, false);
+
+        Location location = new Location(StatsLeaderboardManager.MAIN_LOBBY_SPAWN.getWorld(), 29.5, 81, 165.5, 90, 0);
+        npc.spawn(location);
+
+        HologramDataText hologramDataText = new HologramDataText.Builder<>(ComponentBuilder.create(
+                "Guild Grandmaster",
+                NamedTextColor.GREEN
+        ).build()).setBillboard(Display.Billboard.CENTER).build();
+        HologramManager.addHologram(new Hologram.Builder(
+                        "guildGrandmaster",
+                        location.clone().add(0, 2.1, 0),
+                        player -> hologramDataText
+                ).setVisibility(VisibilityType.ALL).build()
+        );
     }
 
     @EventHandler
